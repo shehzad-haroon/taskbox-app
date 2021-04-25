c8963431f2e687ec08b7667dfdd567ecd7deaaa0
shehzad-haroon <56392887+shehzad-haroon@users.noreply.github.com>
2021-04-26 01:15:54 +0500

    done taskbox app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import Taskbox from './Component/Taskbox';
+import Button from './Component/Button';
+import  {TsBx} from './Component/TsBox';
 
-function App() {
+interface prop{
+  // handleChange:string,
+  label:string
+}
+function App(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h3>Taskbox</h3>
+     {/* <Button value={"hello"}/> */}
+     <TsBx/>
     </div>
   );
 }
---
+++ src/stories/Taskbox.stories.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {TsBx} from '../Component/TsBox'
+
+export default {
+    title:'Taskbox',
+    component:TsBx
+}
+
+export function TsBoxComp() {
+    return(
+        <div> 
+            <TsBx/>
+        </div>
+    )
+}
